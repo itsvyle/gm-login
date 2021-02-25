7b2a7ed7bfa228e021097e8ff453b6448dc932eb
itsvyle <65409906+itsvyle@users.noreply.github.com>
2021-02-25 10:32:08 -0800

    Update example.js

--- example.js
+++ example.js
@@ -2,23 +2,27 @@ var express = require("express");
 const app = express();
 const port = 3000;
 var bodyParser = require('body-parser');
-var Login = require("./login");
 
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb",extended: true}));
 
 
+var GMLogin = require("./gm-login");
 const loginRoute = "/accounts";
-const login = new Login({
+const login = new GMLogin({
     useReplDatabases: true,
     accounts_admin: true,
     onReady: function () {
-        app.listen(3000,function () {
-            console.log("App listening at port: " + String(port)); 
+        console.log("[login] Loaded data");
+        SimpleDB.startMultiple(true,db,dbDistribution).then(() => {
+            console.log("[db] Got all the local db data");
+            client.start(); 
         });
     },
-    flags: {
-        
+    options: {
+        website_name: "Google",
+        website_icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Repl.it_logo.svg/480px-Repl.it_logo.svg.png",
+        dark: false
     }
 });
 
